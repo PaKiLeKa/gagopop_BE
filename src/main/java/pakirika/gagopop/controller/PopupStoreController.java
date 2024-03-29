5a84962002ecfd440ec927b58d08e3ceb8d6490e
Yujin <yudility@gmail.com>
2024-03-29 23:45:15 +0900

    :sparkles: feat: 팝업스토어 목록과 각각의 팝업스토어가 사용자 wishlist에 담겨있는지 여부를 반환하는 API 추가

--- src/main/java/pakirika/gagopop/controller/PopupStoreController.java
+++ src/main/java/pakirika/gagopop/controller/PopupStoreController.java
@@ -1,17 +1,25 @@
 package pakirika.gagopop.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pakirika.gagopop.dto.PopupWishDTO;
 import pakirika.gagopop.entity.PopupStore;
+import pakirika.gagopop.entity.Wishlist;
+import pakirika.gagopop.entity.UserEntity;
+import pakirika.gagopop.jwt.JWTUtil;
+import pakirika.gagopop.repository.UserRepository;
+import pakirika.gagopop.repository.WishlistRepository;
 import pakirika.gagopop.service.PopupStoreService;
 import pakirika.gagopop.repository.PopupStoreRepository;
+import pakirika.gagopop.service.WishlistService;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,17 +28,26 @@ public class PopupStoreController {
 
     private final PopupStoreRepository popupStoreRepository;
     private final PopupStoreService popupStoreService;
+
+    private final UserRepository userRepository;
+
+    private final WishlistRepository wishlistRepository;
+
+    private final WishlistService wishlistService;
+
+    private final JWTUtil jwtUtil;
     
     @GetMapping("popup/find")
-    public List<PopupStore>  findPopupByNameAndDate(@RequestParam("name") String name, @RequestParam("date") String dateString){
+    public ResponseEntity<?> findPopupByNameAndDate(@RequestParam("name") String name,
+                                                    @RequestParam("date") String dateString){
 
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate localDate=LocalDate.parse( dateString, formatter );
-        //Date date=Date.from( localDate.atStartOfDay( ZoneId.systemDefault() ).toInstant() );
-        List<PopupStore> result= popupStoreRepository.findOpenPopupStoresByNameDate( name, localDate );
-
-        return result;
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            LocalDate localDate=LocalDate.parse( dateString, formatter );
+            //Date date=Date.from( localDate.atStartOfDay( ZoneId.systemDefault() ).toInstant() );
+            List<PopupStore> result= popupStoreRepository.findOpenPopupStoresByNameDate( name, localDate );
+            return ResponseEntity.ok(result);
+        //}
     }
 
     @GetMapping("popup/find-route")
@@ -44,7 +61,9 @@ public class PopupStoreController {
     }
 
     @GetMapping("popup/find-all")
-    public List<PopupStore> findPopupAll(){
+    public List<PopupStore> findPopupAll(@RequestHeader(value="Authorization", required=false) String authorizationHeader){
+
+
         List<PopupStore> allPopupStores = popupStoreRepository.findAll();
         // TODO
         // 서비스가 커진 후에 날짜 제한 없이 보내면 데이터가 너무 클 것 같다.
@@ -53,5 +72,61 @@ public class PopupStoreController {
         return allPopupStores;
     }
 
+    @GetMapping("popup/find-all-with-wish")
+    public List<PopupWishDTO> findPopupAllWithWish(@RequestHeader(value = "Authorization", required = false) String authorizationHeader) {
+        List<PopupStore> allPopupStores = popupStoreRepository.findAll();
+
+        List<Long> popupStoreIdsInWishlist = new ArrayList<>();
+
+        if (authorizationHeader != null) {
+            UserEntity userEntity = getUserIdFromAuthorizationHeader(authorizationHeader);
+            if (userEntity != null) {
+                List<Wishlist> wishlists = wishlistRepository.findByUserEntity(userEntity);
+                if (wishlists != null) {
+                    popupStoreIdsInWishlist = wishlists.stream()
+                            .map(Wishlist::getPopupStore)
+                            .filter( Objects::nonNull)
+                            .map(PopupStore::getId)
+                            .collect(Collectors.toList());
+                }
+            }
+        }
+
+        final List<Long> finalPopupStoreIdsInWishlist = popupStoreIdsInWishlist;
+
+        return allPopupStores.stream()
+                .map(popupStore -> {
+                    boolean isInWishlist = false;
+                    if (finalPopupStoreIdsInWishlist != null && finalPopupStoreIdsInWishlist.contains(popupStore.getId())) {
+                        isInWishlist = true;
+                    }
+                    return new PopupWishDTO(isInWishlist, popupStore );
+                })
+                .collect(Collectors.toList());
+    }
+
+
+
+    // Assuming you have a method to extract userId from the authorizationHeader
+    private UserEntity getUserIdFromAuthorizationHeader(String authorizationHeader) {
+        String token = authorizationHeader.replace("Bearer ", "");
+
+        if(token.isEmpty()){
+          return null;
+        }
+        // JWT 토큰에서 유저 이름 가져오기
+        String username = jwtUtil.getUsername(token);
+        if(username.isEmpty()){
+            return null;
+        }
+
+        UserEntity userEntity=userRepository.findByUsername( username );
+        if(userEntity == null){
+            return null;
+        }
+
+
+        return userEntity;
+    }
     
 }
