e7cd57e131b064f4b50c8ccb0f90b8b57f35610c
Yujin <yudility@gmail.com>
2024-03-30 23:03:51 +0900

    :fire: remove: 유저 스템프 리스트 이름 변경으로 인한 불필요한 코드 삭제

--- src/main/java/pakirika/gagopop/repository/StampListRepository.java
+++
@@ -1,9 +0,0 @@
-package pakirika.gagopop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pakirika.gagopop.entity.StampList;
-import pakirika.gagopop.entity.UserEntity;
-
-public interface StampListRepository extends JpaRepository<StampList, Long> {
-    Long countByUserEntity(UserEntity userEntity);
-}
