b3cdb6f900e66cb94421d243e77ebda1ae560c49
Yujin <yudility@gmail.com>
2024-03-31 08:32:17 +0900

    :bug: fix: logout 쿠키 삭제되지 않는 문제 수정

---
+++ src/main/java/pakirika/gagopop/controller/LogoutController.java
@@ -0,0 +1,38 @@
+package pakirika.gagopop.controller;
+
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
+
+@Controller
+public class LogoutController {
+
+    @GetMapping("/api/logout")
+    public void myLogout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //Cookie[] cookies =request.getCookies();
+
+        response.addCookie(deleteCookie("Authorization", ""));
+
+        response.sendRedirect("/");
+    }
+    private Cookie deleteCookie(String key, String value) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(0);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setAttribute( "SameSite", "None" );
+        cookie.setHttpOnly(true);
+
+        return cookie;
+    }
+}
