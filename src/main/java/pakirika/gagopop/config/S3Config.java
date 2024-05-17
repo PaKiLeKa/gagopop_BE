10c4633ba410144183bfbb612d26587e05eb125c
Yujin <yudility@gmail.com>
2024-05-17 23:39:15 +0900

    :fire: remove: s3config 사용하지 않는 코드 제거

--- src/main/java/pakirika/gagopop/config/S3Config.java
+++ src/main/java/pakirika/gagopop/config/S3Config.java
@@ -4,7 +4,6 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -25,14 +24,6 @@ public class S3Config {
     @Value("${cloud.aws.s3.endpoint}")
     private String endPoint;
 
-/*    @Bean
-    public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey,secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, region))
-                .build();
-    }*/
 
     @Bean
     public AmazonS3 s3(){
